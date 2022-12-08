35cecfeed448845a600ea6db19c34c26c293a171
SifaKasena <sifacollins@gmail.com>
2022-12-08 00:58:21 -0800

    [Fix:] Add newline characters and remove trailing whitespace

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	printf("size of char: %ld byte(s)", sizeof(char));
-	printf("size of int: %ld byte(s)", sizeof(int));
-	printf("size of long int: %ld byte(s)", sizeof(long int));	
-	printf("size of long long int: %ld byte(s)", sizeof(long long int));
-	printf("size of float: %ld byte(s)", sizeof(float));
+	printf("size of char: %ld byte(s)\n", sizeof(char));
+	printf("size of int: %ld byte(s)\n", sizeof(int));
+	printf("size of long int: %ld byte(s)\n", sizeof(long int));
+	printf("size of long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("size of float: %ld byte(s)\n", sizeof(float));
 	return (0);
 }
