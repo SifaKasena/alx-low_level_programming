5bf5f53c5b5e7010b74709065ab121a828e7d283
SifaKasena <sifacollins@gmail.com>
2022-12-09 01:53:01 -0800

    [Fix:] Remove trailing white space, fix comment

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 
-/*
+/**
  *main - entry point
  *
  *Return: always (0) success
@@ -10,7 +10,7 @@
 int main(void)
 {
 	int n;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n == 0)
