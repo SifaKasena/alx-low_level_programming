fdb6a993392fe970a516fddc454a77c47f306954
SifaKasena <sifacollins@gmail.com>
2022-12-09 01:49:35 -0800

    [Fix:] Betty style documentation

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,16 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /*
- * main -entry point
+ *main - entry point
  *
- * Return (0) always success
+ *Return: always (0) success
  */
 int main(void)
 {
 	int n;
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n == 0)
