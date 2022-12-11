acbbf6a28478cb22699285c4b4d3de8bbf66fcc0
SifaKasena <sifacollins@gmail.com>
2022-12-11 06:57:19 -0800

    Solve task 10 0f 0x01. C-Variables, if, else, while

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: print unique combinations of double dogit numbers
+ * Return: always (0) success
+ */
+int main(void)
+{
+	int n;
+	int i;
+
+	for (n = '0'; n < '9'; n++)
+	{
+		for (i = '1'; i <= '9'; i++)
+		{
+			if (n != i && i > n)
+			{
+				putchar(n);
+				putchar(i);
+				if (n == '8' && i == '9')
+					break;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+}
