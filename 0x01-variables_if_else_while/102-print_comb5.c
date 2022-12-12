7c95b72489466f5b1278014a5980fe8214d0cc9d
SifaKasena <sifacollins@gmail.com>
2022-12-12 03:40:02 -0800

    [Fix:] Remove extra commas and spaces

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -28,7 +28,7 @@ int main(void)
 						putchar(c);
 						putchar(d);
 					}
-					if (a != '9' && b != '8')
+					if (a != '9' && b != '8' && (c > a || (c == a && d > b)))
 					{
 						putchar(',');
 						putchar(' ');
