47e13f1cb35e9dd2e7a95e633231c4240b4a026d
SifaKasena <sifacollins@gmail.com>
2022-12-13 14:36:39 +0300

    Solve task 6 of 0x02.C-Functions, nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of an integer
+ * Return: returns absolute value of integer
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else 
+		return (n);
+}
