ce90635f470a250f2159ddca7c877077394d4631
SifaKasena <sifacollins@gmail.com>
2022-12-13 14:16:36 +0300

    Solve task 5 of 0x02.C-Functions, nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a given number
+ * Return: 0 - number is 0, +1 number is positive, -1 number is negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (+1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
