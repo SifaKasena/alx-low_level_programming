18dd9d965ed351811b4792e15a7970eaf4c18133
SifaKasena <sifacollins@gmail.com>
2022-12-13 14:01:15 +0300

    Solve task 4 of 0x02.C-Functions, nested loops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks whether a character is an alphabetical character
+ * Return: 1 - true; 0 - false
+ */
+
+int _isalpha(int c)
+{
+	return ((c > 64 && c < 91) || (c > 96 && c < 123));
+}
