3034059d24989c8d8e56784062519f76bc9df8e9
SifaKasena <sifacollins@gmail.com>
2022-12-13 13:51:14 +0300

    Solve task 3 of 0x02.C-Functions, nested loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower - checks whether a character is lowercase or not
+ * Return: 1 - true, 0 - false
+ */
+
+int _islower(int c)
+{
+	return (c > 64 && c <= 90);
+}
