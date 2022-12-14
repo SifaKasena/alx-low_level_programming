d815f0c56581fc15ed205163a672a2effb632bd6
SifaKasena <sifacollins@gmail.com>
2022-12-14 16:15:36 +0300

    [Fix:] Solve task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,16 +7,22 @@
 
 void jack_bauer(void)
 {
-	int h;
-	int m;
+	int h, m;
+	int a, b, c, d;
 
 	for (h = 0; h < 24; h++)
 	{
+		a = h / 10;
+		b = h % 10;
 		for (m = 0; m < 60; m++)
 		{
-			_putchar(h+'0');
+			c = m / 10;
+			d = m % 10;
+			_putchar(a + '0');
+			_putchar(b + '0');
 			_putchar(':');
-			_putchar(m+'0');
+			_putchar(c + '0');
+			_putchar(d + '0');
 		}
 		_putchar('\n');
 	}
