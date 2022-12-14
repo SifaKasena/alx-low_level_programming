d33b1c93e09f15e7ccaec19aa32d5bdb4a526dcf
SifaKasena <sifacollins@gmail.com>
2022-12-14 09:23:23 +0300

    Solve task 11 of 0x02. C-Functions, nested loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * Return - no return value
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n <= 98)
+		{
+			_putchar(n+'0');
+			if (n == 98)
+				break;
+			_putchar(',');
+			_putchar(' ');
+			n++;
+		}
+	}
+	else
+	{
+		while (n >= 98)
+		{
+			_putchar(n+'0');
+			if (n == 98)
+				break;
+			_putchar(',');
+			_putchar(' ');
+			n--;
+		}
+	}
+}
