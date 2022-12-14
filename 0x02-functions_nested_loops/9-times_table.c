d890df570f5aaeeef67612e70b07f8d0ce6d9076
SifaKasena <sifacollins@gmail.com>
2022-12-14 16:47:38 +0300

    [Fix:] Remove extra space

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,16 +17,18 @@ void times_table(void)
 			{
 				_putchar((a * b) / 10 + '0');
 				_putchar((a * b) % 10 + '0');
+				if (a * b == 81)
+					break;
+				_putchar(',');
+				_putchar(' ');
 			}
 			else
 			{
 				_putchar(a * b + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
 			}
-			if (a * b == 81)
-				break;
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
 		}
 		_putchar('\n');
 	}
