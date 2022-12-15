48e3353bc5ef065594f071d0bd8066266fa29db8
SifaKasena <sifacollins@gmail.com>
2022-12-15 21:21:51 +0300

    [Fix:] Add separators

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,16 +9,19 @@
 
 void print_to_98(int n)
 {
-	if (n >= 98)
+	if (n > 98)
 	{
-		do {
-			printf("%d", n--);
-		} while (n > 98);
+		while (n > 98)
+		{
+			printf("%d, ", n--);
+		}
 	}
 	else
 	{
-		do {
-			printf("%d", n++);
-		} while (n < 98);
+		while (n < 98)
+		{
+			printf("%d, ", n++);
+		}
 	}
+	printf("%d", 98);
 }
