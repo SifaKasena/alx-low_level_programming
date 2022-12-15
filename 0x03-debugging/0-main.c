8370b6fe3e7c694eb5d029a75c9064da8565cc28
SifaKasena <sifacollins@gmail.com>
2022-12-15 12:51:50 +0300

    SOlve task 0 of 0x03. C - Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests the function that prints whether an int is negative or positive
+ * Return: (0) success
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
