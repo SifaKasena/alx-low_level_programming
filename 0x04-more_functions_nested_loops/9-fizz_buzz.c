96eb2c285da28f5f27c62de65c169572dcae6f32
SifaKasena <sifacollins@gmail.com>
2022-12-15 20:32:23 +0300

    Solve task 9 of 0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: The fizz buzz test
+ * Return: always (0) success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+			printf("FizzBuzz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", i);
+	}
+
+	return (0);
+}
