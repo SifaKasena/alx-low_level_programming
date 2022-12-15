ec0298efad9b59c7e91023b70a17f57d9e57e2a3
SifaKasena <sifacollins@gmail.com>
2022-12-15 21:13:34 +0300

    [Fix:] Use printf

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - prints all natural numbers from n to 98
@@ -8,28 +9,16 @@
 
 void print_to_98(int n)
 {
-	if (n <= 98)
+	if (n >= 98)
 	{
-		while (n <= 98)
-		{
-			_putchar(n);
-			if (n == 98)
-				break;
-			_putchar(',');
-			_putchar(' ');
-			n++;
-		}
+		do {
+			printf(n--);
+		} while (n > 98);
 	}
 	else
 	{
-		while (n >= 98)
-		{
-			_putchar(n);
-			if (n == 98)
-				break;
-			_putchar(',');
-			_putchar(' ');
-			n--;
-		}
+		do {
+			printf(n++);
+		} while (n < 98);
 	}
 }
