3b1e150b1afc454b4cd91937b6a6efc04c8de5f5
SifaKasena <sifacollins@gmail.com>
2022-12-15 14:40:37 +0300

    Solve task 0 of 0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - checks whether a character is ppercase
+ * @c: character to be checked
+ * Return: 1 - true; 0 - false
+ */
+
+int _isupper(int c)
+{
+	return (c > 96 && c < 123);
+}
