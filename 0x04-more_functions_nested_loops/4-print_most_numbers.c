bcf64a8ae8e997032df6dadf6fe750158a828fc1
SifaKasena <sifacollins@gmail.com>
2022-12-16 21:54:33 +0300

    [Fix:] Use for loop

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,13 +8,12 @@
 
 void print_most_numbers(void)
 {
-	int n = '0';
+	int n;
 
-	while (n <= '9')
+	for (n = 0; n < 10; n++)
 	{
-		if (n == '2' || n == '4')
+		if (n == 2 || n == 4)
 			continue;
-		_putchar(n++);
 	}
 	_putchar('\n');
 }
