55f15aef37ac415146dfdd3fd66dcc4611ab82a5
SifaKasena <sifacollins@gmail.com>
2022-12-16 21:46:32 +0300

    [Fix:] Print newline at the end

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,7 +13,9 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 15 == 0)
+		if (i == 100)
+			printf("Buzz\n");
+		else if (i % 15 == 0)
 			printf("FizzBuzz ");
 		else if (i % 5 == 0)
 			printf("Buzz ");
