32cdf91d4e7f8f3d6a24b09850e2b85148008bbc
SifaKasena <sifacollins@gmail.com>
2022-12-19 17:53:06 +0300

    [Fix:] Incude starndard library

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_array - prints n elements of an integer array
@@ -14,7 +15,7 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", *(a + i));
-		if (i = n - 1)
+		if (i == n - 1)
 			break;
 		printf(", ");
 	}
