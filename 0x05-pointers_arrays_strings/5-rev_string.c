b1702bfc2ee753c63033d1820aea04a80e29e96c
SifaKasena <sifacollins@gmail.com>
2022-12-19 16:59:11 +0300

    [Fix:] Use a copy

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,12 +10,15 @@ void rev_string(char *s)
 {
 	int len = 0;
 	int i;
+	char *s_cpy;
 
 	while (*(s + len) != '\0')
 		len++;
 	for (i = 0; i < len; i++)
+		*(s_cpy + i) = *(s + i);
+	for (i = 0; i < len; i++)
 	{
-		*(s + i) = *(s + len - 1);
+		*(s + i) = *(s_cpy + len - 1);
 		len--;
 	}
 }
