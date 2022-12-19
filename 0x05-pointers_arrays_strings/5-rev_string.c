b79c8619830e9100cb7806018f4cace8b3f27ef7
SifaKasena <sifacollins@gmail.com>
2022-12-19 16:55:00 +0300

    Solve task 5 of 0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int len = 0;
+	int i;
+
+	while (*(s + len) != '\0')
+		len++;
+	for (i = 0; i < len; i++)
+	{
+		*(s + i) = *(s + len - 1);
+		len--;
+	}
+}
