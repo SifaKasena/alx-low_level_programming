d8d1bb606c054c3309e8697085d4c03d23228c52
SifaKasena <sifacollins@gmail.com>
2022-12-19 16:36:43 +0300

    Solve task 3 of 0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a newline
+ * @str: string to be printed
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
