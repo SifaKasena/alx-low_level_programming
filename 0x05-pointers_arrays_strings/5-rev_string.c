0a6bb698443bba6247e5dca6baf365d7ec1fc2d5
SifaKasena <sifacollins@gmail.com>
2022-12-19 17:27:59 +0300

    Solve task 6 of 0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,15 +10,16 @@ void rev_string(char *s)
 {
 	int len = 0;
 	int i;
-	char *s_cpy;
-
+	
 	while (*(s + len) != '\0')
 		len++;
+
+	char s_cpy[len];
+
 	for (i = 0; i < len; i++)
 		*(s_cpy + i) = *(s + i);
 	for (i = 0; i < len; i++)
 	{
-		*(s + i) = *(s_cpy + len - 1);
-		len--;
+		*(s + i) = *(s_cpy + len - i - 1);
 	}
 }
