f310970522d21885681db4ba7e9853b86a9d5313
SifaKasena <sifacollins@gmail.com>
2022-12-19 17:42:37 +0300

    [Fix:] Add newline and consider strings with even length

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,10 +8,16 @@
 
 void puts_half(char *str)
 {
-	int i, len = 0;
-	
+	int i, start;
+	int len = 0;
+
 	while (*(str + len) != '\0')
 		len++;
-	for (i = (len / 2) + 1; i < len; i++)
+	if (len % 2 == 0)
+		start = len / 2;
+	else
+		start = (len / 2) + 1;
+	for (i = start; i < len; i++)
 		_putchar(*(str + i));
+	_putchar('\n');
 }
