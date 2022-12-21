8b44a8aeaead3d9a375ffffa7910c0f006d6d687
SifaKasena <sifacollins@gmail.com>
2022-12-21 19:20:51 +0300

    [Fix:] Stop stack smashing

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,10 +11,14 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	do {
-		*(dest + i) = *(src + i);
-		i++;
-	} while (*(src + i) != '\0');
+	if (*src != '\0')
+	{
+		while (*(src + i) != '\0')
+		{
+			*(dest + i) = *(src + i);
+			i++;
+		}
+	}
 	dest[i] = '\0';
 
 	return (dest);
