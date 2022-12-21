d151d13d14ee40b6857da4894a5260f0d72bbe6e
SifaKasena <sifacollins@gmail.com>
2022-12-21 19:16:39 +0300

    [Fix:] Add null byte

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,7 +14,8 @@ char *_strcpy(char *dest, char *src)
 	do {
 		*(dest + i) = *(src + i);
 		i++;
-	} while (*(src + i) != 0);
+	} while (*(src + i) != '\0');
+	dest[i] = '\0';
 
 	return (dest);
 }
