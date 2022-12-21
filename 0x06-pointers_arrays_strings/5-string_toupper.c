7b80bcae6c674df3b32c6eeb5b08f584838005d6
SifaKasena <sifacollins@gmail.com>
2022-12-21 12:20:09 +0300

    Solve task 5 of 0x06. C - More pointers, arrays, strings

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * strint_toupper - changes all lowercase letters of a string to upper
+ * @s: string to be changed
+ * Return: pointer to resulting string
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] += 32;
+		i++;
+	}
+
+	return (s);
+}
