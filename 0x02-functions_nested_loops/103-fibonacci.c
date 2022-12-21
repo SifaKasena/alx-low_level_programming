b7cda002bc2e875f62647bde81eaaed5a8ee8698
SifaKasena <sifacollins@gmail.com>
2022-12-21 22:58:25 +0300

    Solve task 15 of 0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: Prints the first 50 fibonacci numbers
+ * Return: always 0(success)
+ */
+
+int main(void)
+{
+	long i, prev, next, curr, sum;
+
+	prev = 1;
+	next = 2;
+	sum = next;
+	printf("%ld, %ld, ", prev, next);
+	for (i = 1; next + prev <= 4000000; i++)
+	{
+		curr = next + prev;
+		if (i % 2 == 0)
+			sum += curr;
+		prev = next;
+		next = curr;
+	}
+	printf("\n");
+
+	return (0);
+}
