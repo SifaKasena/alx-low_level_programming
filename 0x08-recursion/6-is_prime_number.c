3589388b80e9bbc20368b5008244d31a26f5f539
SifaKasena <sifacollins@gmail.com>
2023-01-07 16:58:08 +0300

    Solve task 6 of 0x08. C - Recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,5 +8,37 @@
 
 int is_prime_number(int n)
 {
-	return (0*n);
+	int x = 2;
+
+	if (n < 2)
+		return (0);
+	else if (n <= 3)
+		return (1);
+	else
+		return (divisible(&x, n));
+}
+
+/**
+ * divisible - checks whether n is divisible by any number
+ * between 2 and itself
+ * @x: pointer to result
+ * @n: number to be checked
+ */
+
+int divisible(int *x, int n)
+{
+	if (*x == n)
+	{
+		*x = 1;
+	}
+	else if (n % *x == 0)
+	{
+		*x = 0;
+	}
+	else
+	{
+		*x += 1;
+		divisible(x, n);
+	}
+	return (*x);
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,5 +10,6 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int square(int *x, int n);
 int is_prime_number(int n);
+int divisible(int *x, int n);
 
 #endif
