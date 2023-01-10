0025482155b559efaa296352e6d864b0ffad5975
SifaKasena <sifacollins@gmail.com>
2023-01-10 08:21:40 +0300

    [Fix:]

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,11 +11,19 @@
 
 int main(int argc, char **argv)
 {
-	int i, sum = 0;
+	int i, j, state, sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		if (argv[i][0] < '0' || argv[i][0] > '9')
+		state = 1;
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+				state = 0;
+			j++;
+		}
+		if (!state)
 		{
 			printf("Error\n");
 			return (1);
